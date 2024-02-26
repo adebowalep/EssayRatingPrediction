5e703ad9c0609f448939d644fc8c48edb2439292
Anthony Reichen <anthonyreichen@gmail.com>
2024-02-26 18:36:43 +0100

    :sparkles: add features in a separate module

--- src/features.py
+++ src/features.py
@@ -1,81 +1,153 @@
-# Librairies
-
-from nltk.corpus import stopwords
-from nltk import RegexpTokenizer, pos_tag
-from nltk.tokenize import sent_tokenize
-from nltk.corpus import cmudict
+import textstat
 import nltk
 import json
+import string
 import pyphen
-import spacy
-import re
+
+
+from nltk import sent_tokenize, word_tokenize, pos_tag, RegexpParser
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.corpus import words
+from nltk.corpus import cmudict
+
+
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('words')
+
+dict_cmu = cmudict.dict()
 
 pyphen.language_fallback('en_GB')
 dic = pyphen.Pyphen(lang='en_GB')
-
-dict_cmu = cmudict.dict()
 
 with open("./utils/dict_awl.json") as file:
     dict_awl = json.load(file)
 list_awl = [item for sublist in dict_awl.values() for item in sublist]
 set_awl = set(list_awl)
 
-nlp = spacy.load("en_core_web_sm")
+with open("./utils/set_slur_words.json") as file:
+    list_slur_words = json.load(file)
+    set_slur_words = set(list_awl)
 
+set_tags = set(['LS', 'TO', 'VBN', 'WP', 'UH', 'VBG', 'JJ', 'VBZ', 'VBP', 'NN', 'DT', 'PRP', 
+                'WP$', 'NNPS', 'PRP$', 'WDT', 'RB', 'RBR', 'RBS', 'VBD', 'IN', 'FW', 'RP', 'JJR',
+                  'JJS', 'PDT', 'MD', 'VB', 'WRB', 'NNP', 'EX', 'NNS', 'SYM', 'CC', 'CD', 'POS'])
 
-with open("./utils/dale-chall-word-list.txt", "r") as file:
-    array_dale_chall = file.read().splitlines()
+english_words = set(words.words())
 
-array_dale_chall =[word.replace(" ", "") for word in array_dale_chall]
-set_dale_chall = set(array_dale_chall)
+def get_flesch_reading_ease(text: str):
+    return textstat.flesch_reading_ease(text)
 
-with open("./utils/words_alpha.txt") as file:
-    lines = file.readlines()
+def get_gunning_fog(text: str):
+    return textstat.gunning_fog(text)
 
-lines_clean = [line.replace("\n", "").lower() for line in lines]
-set_word_check = set(lines_clean)
+def get_automated_readability_index(text: str):
+    return textstat.automated_readability_index(text)
 
-cached_stopwords = set(stopwords.words("english"))
-# Preprocessing
+def get_smog_index(text: str):
+    return textstat.smog_index(text)
 
-def tokenize_essay(essay):
-    return RegexpTokenizer(r"[a-zA-Z0-9]+").tokenize(str.lower(essay))
+def get_flesch_kincaid_grade(text: str):
+    return textstat.flesch_kincaid_grade(text)
 
-def lemmatize_essay(essay):
-    text = nlp(essay, disable=["tok2vec", "tagger", "parser", "attribute_ruler", "ner", "tokenizer", "textcat"])
-    return [token.lemma_ for token in text]
+def get_coleman_liau_index(text: str):
+    return textstat.coleman_liau_index(text)
 
+def get_dale_chall_readability_score(text: str):
+    return textstat.dale_chall_readability_score(text)
 
-def remove_stop_words(tokens):
-    output = [word for word in tokens if word not in cached_stopwords]
-    return output
+def get_automated_readability_index(text: str):
+    return textstat.automated_readability_index(text)
 
-def remove_punctuation(essay):
-    essay = essay.replace("/", " ")
-    essay = essay.replace("(", " ")
-    essay = essay.replace(")", " ")
-    essay = essay.replace("  ", " ")
-    return re.sub(r'[^\w\s]', '', essay)
+def get_difficult_words(text: str):
+    return textstat.difficult_words(text)
 
-def remove_special_words(essay):
-    return re.sub(r'@\w+', '', essay)
+def get_linsear_write_formula(text: str):
+    return textstat.linsear_write_formula(text)
 
-# Words and word structure
+def count_awl_words(tokens: list):
+    return len([word for word in tokens if word in set_awl])
 
-## Flesch reading ease --> numbers of words, numbers of sentences, numbers of syllables
-## https://en.wikipedia.org/wiki/Flesch–Kincaid_readability_tests
-## For Flesch kincaid score, we don't remove stopwords
+def calculate_lexical_diversity(text):
+    words = text.split()
+    unique_words = set(words)
+    return len(unique_words) / len(words)
 
-def count_words(tokens):
-    return(len(tokens))
+def get_pos_tags(text):
+    tokens = word_tokenize(text)
+    count_words = len(tokens)
+    dict_pos_tags = {}
+    pos_tags = nltk.pos_tag(tokens)
+    res = [x[1] for x in pos_tags]
+    for a in set_tags:
+        dict_pos_tags.update({a: res.count(a)/count_words})
+    dict_pos_tags = dict(sorted(dict_pos_tags.items()))
+    return dict_pos_tags
 
-def count_sentences(essay):
-    return len(sent_tokenize(essay))
+def get_sentence_tree_roots(text):
+    words = word_tokenize(text)
+    pos_tags = pos_tag(words)
+    grammar = "NP: {<DT>?<JJ>*<NN>}"
+    chunk_parser = RegexpParser(grammar)
+    tree = chunk_parser.parse(pos_tags)
+    roots = [subtree.label() for subtree in tree.subtrees() if subtree.label() == 'NP']
+    return roots
+
+def get_sentence_tree_length(text):
+    sentences = sent_tokenize(text)
+    return len(sentences)
+
+def get_average_connections_at_root(text):
+    words = word_tokenize(text)
+    pos_tags = pos_tag(words)
+    grammar = "NP: {<DT>?<JJ>*<NN>}"
+    chunk_parser = RegexpParser(grammar)
+    tree = chunk_parser.parse(pos_tags)
+    root_connections = [len(subtree) for subtree in tree.subtrees() if subtree.label() == 'NP']
+    return sum(root_connections) / len(root_connections) if root_connections else 0
+
+def get_length_of_clauses(text):
+    words = word_tokenize(text)
+    pos_tags = pos_tag(words)
+    grammar = "CL: {<RB>?<VB.*><DT>?<JJ>*<NN>}"
+    chunk_parser = RegexpParser(grammar)
+    tree = chunk_parser.parse(pos_tags)
+    clauses_lengths = [len(subtree) for subtree in tree.subtrees() if subtree.label() == 'CL']
+    return sum(clauses_lengths) / len(clauses_lengths) if clauses_lengths else 0
+
+def calculate_misspelling_score(text):
+    tokens = word_tokenize(text.lower())
+    # Extract tokens that are not in the English words set
+    misspelled_tokens = [token for token in tokens if token not in english_words]
+    # Calculate misspelling score as the ratio of misspelled tokens to total tokens
+    misspelling_score = len(misspelled_tokens) / len(tokens) if len(tokens) > 0 else 0
+    return misspelling_score
+
+def detect_slur_usage(text):
+    # You can customize this list with slurs or offensive words
+    tokens = word_tokenize(text.lower())
+    slur_count = sum(1 for token in tokens if token in set_slur_words)
+    return slur_count
+
+def calculate_overusage_of_punctuation(text):
+    tokens = word_tokenize(text)
+    # Extract punctuation tokens
+    punctuation_tokens = [token for token in tokens if token in string.punctuation]
+    # Calculate overusage of punctuation as the ratio of punctuation tokens to total tokens
+    overusage_score = len(punctuation_tokens) / len(tokens) if len(tokens) > 0 else 0
+    return overusage_score
+
+def count_characters(text):
+    text = text.replace(" ", "")
+    return len(text)
 
 def _nsyl(word):
   return [len(list(y for y in x if y[-1].isdigit())) for x in dict_cmu[word.lower()]]
 
-def count_syllables(tokens):
+def count_syllables(text):
+    tokens = word_tokenize(text)
     nb_syl = 0
     for word in tokens:
         try:
@@ -86,95 +158,10 @@ def count_syllables(tokens):
             nb_syl += len(string)
     return nb_syl
 
-## Gunning fog --> complex words ("complex" words consisting of three or more syllables. Do not include proper nouns, familiar jargon, or compound words. Do not include common suffixes (such as -es, -ed, or -ing) as a syllable;)
-## https://en.wikipedia.org/wiki/Gunning_fog_index
+def count_words(text):
+    tokens = word_tokenize(text)
+    return len(tokens)
 
-def count_complex_words(tokens): ## à revoir pour améliorer la performance
-    # nb_complex_words = 0
-    # for word in tokens:
-    #     if count_syllables(word) >= 3:
-    #         nb_complex_words += 1
-    #return nb_complex_words
-    return len([word for word in tokens if count_syllables(word)>=3])
-
-
-
-## Automated Readability Index --> number of characters
-## https://en.wikipedia.org/wiki/Automated_readability_index
-
-def count_characters(essay):
-    essay = essay.replace(" ", "")
-    return len(essay)
-
-## SMOG --> Polysyllables (= words of 3 or more syllables, maybe similar to Gunning fog's "complex words") 
-## https://en.wikipedia.org/wiki/SMOG
-
-## Flesch kincaid : same factors than Flesch reading ease
-
-## Coleman-Liau index --> average number of letters per 100 words, and average number of sentences per 100 words (maybe irrelevant since we already have numbers of letters and sentences)
-## https://en.wikipedia.org/wiki/Coleman–Liau_index
-
-## Dale-Chall readability formula --> list of difficult words (set of 3 000  familiar words)
-
-def count_dale_chall_difficult_words(tokens):
-    difficult_words = [word for word in tokens if word not in set_dale_chall]
-    return len(difficult_words)
-
-
-# Lexically
-
-## Words sophistication thanks to a corpus (AWL)
-
-def count_awl_words(tokens):
-    return len([word for word in tokens if word in set_awl])
-
-## Words frequency (tf-idf)
-## I'm not sure we should use this measure, beaucause it seems field related
-
-## Lexical diversity
-
-def count_unique_lemme(tokens):
-    return len(set(tokens))
-
-## Lexical variation
-set_tags = set(['LS', 'TO', 'VBN', 'WP', 'UH', 'VBG', 'JJ', 'VBZ', 'VBP', 'NN', 'DT', 'PRP', 
-                'WP$', 'NNPS', 'PRP$', 'WDT', 'RB', 'RBR', 'RBS', 'VBD', 'IN', 'FW', 'RP', 'JJR',
-                  'JJS', 'PDT', 'MD', 'VB', 'WRB', 'NNP', 'EX', 'NNS', 'SYM', 'CC', 'CD', 'POS'])
-
-def get_pos_tags(tokens):
-    dict_pos_tags = {}
-    pos_tags = nltk.pos_tag(tokens)
-    res = [x[1] for x in pos_tags]
-    for a in set_tags:
-        dict_pos_tags.update({a: res.count(a)})
-    dict_pos_tags = dict(sorted(dict_pos_tags.items()))
-    return dict_pos_tags
-
-
-# Syntactic structure
-
-## Roots of Sentence tree
-
-## Lenght of Sentence tree
-
-## Average number of Connections of Sentence tree at the root level
-
-## Lenght of clauses
-
-# Quality
-
-## Mispelling score
-
-def get_incorrect_words(tokens):
-    return [word for word in tokens if word not in set_word_check]
-
-def count_incorrect_words(tokens):
-    return len([word for word in tokens if word not in set_word_check])
-
-## Slur usage
-
-# Maybe a lack of data (hard to find wordlist of slur)
-
-## Overusage of punctuation
-
-# Count the number of punctuation
+def count_sentences(text):
+    sentences = sent_tokenize(text)
+    return len(sentences)
